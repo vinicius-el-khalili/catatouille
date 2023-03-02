b8cacd15b2d31e89bb3437d7fbd237da08b5de70
vinicius-el-khalili <velkhaliliborsato@gmail.com>
2023-03-02 00:28:58 -0300

    Layout.js & Layout.module.css

---
+++ pages/components/Layout.js
@@ -0,0 +1,31 @@
+import Link from "next/link"
+import style from "@/styles/Layout.module.css"
+import Image from "next/image";
+const Layout = ({children}) => {
+    return (
+        <div className={style.layout}>
+            <header>
+                <Link href={"/"} className={style.pageHeader}>
+                    <div className={style.pageTitle}>
+                        <h1>THE</h1>
+                        <h1>CATATOUILLE</h1>
+                        <h1>routine cousine</h1>
+                    </div>
+                    <div className={style.catatouille}>
+                        <Image src={"/cat.svg"} fill alt="logo" priority={true} style={{objectFit:"cover"}} />
+                    </div>
+                </Link>     
+            </header>
+
+            <div className={style.pageContent}>
+                {children}
+            </div>
+            <footer className={style.pageFooter}>
+                <p>Footer</p>
+
+            </footer>
+        </div>
+    );
+}
+ 
+export default Layout;
